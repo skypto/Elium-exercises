fc2fe830ce3b40bf0b4c8a153512c24560be5b5c
Turfie <laurens.dewaele@gmail.com>
2017-05-12 11:30:05 +0200

    added 1 more

---
+++ strings_remove_exclamation_marks.js
@@ -0,0 +1,18 @@
+/*
+
+Aryanto keeps swearing.
+We're going to make his sentences a little less harmful by removing
+every exclamation mark and converting all characters to lowercase.
+
+*/
+
+function makeItGentler(str) {
+
+}
+
+
+
+console.log(makeItGentler('FUCK I HATE THIS SHIT!!!!'))
+// outputs fuck i hate this shit
+console.log(makeItGentler('I\'M GOING TO KILL MYSELF!!!!!!!!!'))
+// outputs i'm going to kill myself
