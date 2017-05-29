a0cdd2060dd1c1f54e003d64f7d1eb7de9d265e7
Turfie <laurens.dewaele@gmail.com>
2017-05-29 08:42:26 +0200

    added exercises for week 3

---
+++ week 3/day1/0_example.js
@@ -0,0 +1,2 @@
+var textToChange = 	document.getElementById('change-me')
+textToChange.innerHTML = 'This text was changed by Javascript. Look at the .js file'
---
+++ week 3/day1/3_values_input_field.js
@@ -0,0 +1,1 @@
+console.log(student1, student2)
